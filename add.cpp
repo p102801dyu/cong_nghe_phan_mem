79e52b36163a3a1a115a6ecf7fc80e92a368b48a
Duy Nguyen <duy02102000@gmail.com>
2020-05-18 20:49:08 +0700

    first commit

---
+++ add.cpp
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int Addition(int a,int b){
+    return(a+b);
+}
+
+int main(){
+    int a = 3;
+    int b = 4;
+    cout << "tong hai so la : "<<Addition(a,b);
+
+}
